32c3c3a21800d1c7759c9479a599e916293e3723
wallacevilela7 <wallace.vilela9@gmail.com>
2025-04-10 17:58:32 -0300

    initial commit: set up smartstock application with Spring Boot, MongoDB, and basic structure

---
+++ src/main/java/tech/wvs/smartstockapp/SmartstockAppApplication.java
@@ -0,0 +1,13 @@
+package tech.wvs.smartstockapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartstockAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartstockAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/wvs/smartstockapp/SmartstockAppApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.wvs.smartstockapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartstockAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
