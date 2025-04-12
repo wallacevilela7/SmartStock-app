ec3194072bb8f573e5c4b22e5beeacd5898830c8
wallacevilela7 <wallace.vilela9@gmail.com>
2025-04-11 22:33:15 -0300

    feat: enhance SmartStockService to persist purchase requests in MongoDB

--- src/main/java/tech/wvs/smartstockapp/service/SmartStockService.java
+++ src/main/java/tech/wvs/smartstockapp/service/SmartStockService.java
@@ -2,8 +2,11 @@ package tech.wvs.smartstockapp.service;
 
 import org.springframework.stereotype.Service;
 import tech.wvs.smartstockapp.domain.CsvStockItem;
+import tech.wvs.smartstockapp.entity.PurchaseRequestEntity;
+import tech.wvs.smartstockapp.repository.PurchaseRequestRepository;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -11,11 +14,13 @@ public class SmartStockService {
 
     private final PurchaseSectorService purchaseSectorService;
     private final ReportService reportService;
+    private final PurchaseRequestRepository purchaseRequestRepository;
     private final Double REORDER_SECURITY_PERCENTAGE = 0.20;
 
-    public SmartStockService(PurchaseSectorService purchaseSectorService, ReportService reportService) {
+    public SmartStockService(PurchaseSectorService purchaseSectorService, ReportService reportService, PurchaseRequestRepository purchaseRequestRepository) {
         this.purchaseSectorService = purchaseSectorService;
         this.reportService = reportService;
+        this.purchaseRequestRepository = purchaseRequestRepository;
     }
 
     public void start(String reportPath) {
@@ -32,8 +37,11 @@ public class SmartStockService {
                     var reorderQuantity = calculateReorderQuantity(item);
 
                     // 2.2 fazer a requisição para a api de compras do smartstock
-                    purchaseSectorService.sendPurchaseRequest(item, reorderQuantity);
+                    var purchasedWithSuccess = purchaseSectorService.sendPurchaseRequest(item, reorderQuantity);
 
+
+                    // 3. salvar cada 'item' comprado no mongodb
+                    persist(item, reorderQuantity, purchasedWithSuccess);
                 }
             });
 
@@ -41,9 +49,26 @@ public class SmartStockService {
                 IOException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    private void persist(CsvStockItem item,
+                         Integer reorderQuantity,
+                         boolean purchasedWithSuccess) {
 
-        // 3. salvar cada 'item' comprado no mongodb
+        var entity = new PurchaseRequestEntity();
+        entity.setItemId(item.getItemId());
+        entity.setItemName(item.getItemName());
+        entity.setQuantityInStock(item.getQuantity());
+        entity.setReorderThreshold(item.getReorderThreshold());
+        entity.setSupplierName(item.getSupplierName());
+        entity.setSupplierEmail(item.getSupplierEmail());
+        entity.setLastStockUpdateTime(LocalDateTime.parse(item.getLastStockUpdateTime()));
+
+        entity.setPurchaseQuantity(reorderQuantity);
+        entity.setPurchasedWithSuccess(purchasedWithSuccess);
+        entity.setPurchaseDateTime(LocalDateTime.now());
+
+        purchaseRequestRepository.save(entity);
     }
 
     private Integer calculateReorderQuantity(CsvStockItem item) {
